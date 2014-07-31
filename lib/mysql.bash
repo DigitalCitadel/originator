e9dcce8dd93e8a868c1bac4ffaa1c6e2454f9328
Brandon Romano <BrandonRRomano@gmail.com>
2014-07-31 14:41:20 -0400

    Added function to check if table exists.

--- lib/mysql.bash
+++ lib/mysql.bash
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+#################################################
 # Executes a database statement
 #
 # @param $1: the statement to be executed
+#################################################
 database_execute() {
     mysql   --user="$MYSQL_USER" \
             --password="$MYSQL_PASS" \
             --database="$MYSQL_DATABASE" \
             --execute="$1"
+}
+
+#################################################
+# Check if a database table exists
+#
+# @param $1: the table to check existence
+#################################################
+database_table_exists() {
+    var=$(database_execute "SHOW TABLES LIKE '$1'")
+    if [ "$var" = "" ]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
