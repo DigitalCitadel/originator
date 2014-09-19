41b27d540e9a0f58688029dd8fcc181267702cb1
BrandonRomano <BrandonRRomano@gmail.com>
2014-09-19 16:54:04 -0400

    Can now backup the database.

--- modules/backup/lib/backup.bash
+++ modules/backup/lib/backup.bash
@@ -4,7 +4,28 @@
 # Backs up the entire database
 #################################################
 Backup__index() {
-    echo "Backup__index"
+    tables=$(Database__get_tables);
+
+    words=( $tables )
+    if [ ${#words[@]} -gt 0 ]; then
+
+        # Preparing backup directory
+        timestamp=$(date +%s)
+        backup_dir="./backups/$timestamp"
+        mkdir "$backup_dir"
+        mkdir "$backup_dir/tables"
+        last_migration=$(Database__get_most_recent_migration)
+        echo "$last_migration" > "$backup_dir/last_migration.txt"
+
+        # Backing up each file
+        for table in $tables
+        do
+            file="$backup_dir/tables/$table.sql"
+            Database__backup_table "$table" "$file"
+        done
+    else
+        Logger__alert "There are no tables to backup"
+    fi
 }
 
 #################################################
