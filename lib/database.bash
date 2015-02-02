1fc3cb6d77e5d1d5084022440b84d7a255a7bab0
Brandon Romano <BrandonRRomano@gmail.com>
2015-02-02 08:34:28 -0500

    Adding postgres base database driver

--- lib/database.bash
+++ lib/database.bash
@@ -9,6 +9,8 @@ Database__setup() {
     if [[ "$Database__driver" = 'mysql' ]]; then
         . "$Originator__lib_directory"/lib/database_drivers/mysql.bash
         Mysql__generate_config
+    elif [[ "$Database__driver" = 'postgres' ]]; then
+        . "$Originator__lib_directory"/lib/database_drivers/postgres.bash
     fi
 }
 
@@ -17,7 +19,7 @@ Database__setup() {
 # Shuts down the database
 #################################################
 Database__shutdown() {
-    # Loading the appropriate database driver
+    # Removing the config file
     if [[ "$Database__driver" = 'mysql' ]]; then
         Mysql__delete_config
     fi
